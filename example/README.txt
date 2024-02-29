20f6dfdc661ab8d27311f07830ce6ef3be50422d
Mika Mäki <agentti002x@gmail.com>
2024-02-29 23:44:36 +0200

    Add names for the project

--- example/README.txt
+++ example/README.txt
@@ -9,9 +9,7 @@
 A very small example that you can examine and modify to build your
 very first browser 4k intro.
 
-Author:     <Your own (nick)name here>
-
-            <and contact details, if you want to provide some>
+Author:     Mika "AgenttiX" Mäki
 
 Techniques: lmad1 workshop code (Javascript, WebGL, SoundBox, Closure
             Compiler, Pnginator)
--- example/Settings.mk
+++ example/Settings.mk
@@ -5,13 +5,13 @@
 ## Change these to match your own production:
 
 # A short name of the production, used in generated file names (don't use spaces!)
-PROD_NAME=example
+PROD_NAME=visions
 
 # A "full" name of the production, used in zip package name (don't use spaces!)
-PROD_NAME_FULL=Example_4k
+PROD_NAME_FULL=Visions_4k
 
 # Your handle/nickname, also included in the zip package name (don't use spaces!)
-PROD_AUTHOR=The_Old_Dude
+PROD_AUTHOR=Mika_Mäki
 
 ## Change these to match your local installation directories...
 #
